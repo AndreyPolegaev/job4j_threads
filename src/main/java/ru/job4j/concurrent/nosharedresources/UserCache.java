00efd762a932735df39ca19123b01220a6f17ae1
AndreyPolegaev <79646307753@yandex.ru>
2021-09-21 17:25:13 +0300

    Thread без общих ресурсов

---
+++ src/main/java/ru/job4j/concurrent/nosharedresources/UserCache.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent.nosharedresources;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import net.jcip.annotations.ThreadSafe;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+
+    }
+
+    public List<User> findAll() {
+        return List.copyOf(users.values());
+    }
+}
