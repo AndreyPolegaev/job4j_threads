105c0ed7390dec9a10e0c6b4c93152847d69c61b
AndreyPolegaev <79646307753@yandex.ru>
2021-09-21 12:46:53 +0300

    Скачивание файла с ограничением

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,33 +8,37 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
+    long delay = 0;
 
-    public Wget(String url, int speed) {
+
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
-        long delay = 0;
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("Wget.txt")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             Long start = System.currentTimeMillis();
-            int bytesRead = in.read(dataBuffer, 0, 1024);
-            Long finish = System.currentTimeMillis();
-            if ((finish - start) < speed) {
-                delay = (finish - start) + 1000;
-            }
-            while (bytesRead != -1) {
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                Long finish = System.currentTimeMillis();
+                if ((finish - start) < speed) {
+                    this.delay++;
+                    run();
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                bytesRead = in.read(dataBuffer, 0, 1024);
+                fileOutputStream.flush();
                 try {
                     Thread.sleep(delay);
                 } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                     e.printStackTrace();
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,9 +46,17 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String url;
+        int speed;
+        String fileName;
+        try {
+            url = args[0];
+            speed = Integer.parseInt(args[1]);
+            fileName = args[2];
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("некорректные данные");
+        }
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
