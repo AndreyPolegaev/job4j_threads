66eaf8727a16ff4b5e8a65a93a87f031847ee8d2
AndreyPolegaev <79646307753@yandex.ru>
2021-10-01 00:01:29 +0300

    others changes

---
+++ src/test/java/ru/job4j/concurrent/producerconsimer/SimpleBlockingQueueTest3.java
@@ -0,0 +1,57 @@
+package ru.job4j.concurrent.producerconsimer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest3 {
+
+    @Test
+    public void whenSeveralThreads() throws InterruptedException {
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread[] threads = new Thread[15];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                try {
+                    queue.offer(10);
+                    System.out.println(Thread.currentThread().getName() + " задача добавлена");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        Thread[] threadConsumer = new Thread[5];
+
+        for (int i = 0; i < threadConsumer.length; i++) {
+            threadConsumer[i] = new Thread(() -> {
+                try {
+                    while ((!queue.isEmpty()) || !Thread.currentThread().isInterrupted()) {
+                        System.out.println(queue.poll());
+                        System.out.println(Thread.currentThread().getName() + " Задача снята");
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+
+        }
+
+        for (var temp : threads) {
+            temp.start();
+          //  temp.join();
+
+        }
+        for (var temp : threadConsumer) {
+            temp.start();
+            temp.interrupt();
+            temp.join();
+
+        }
+
+    }
+
+}
